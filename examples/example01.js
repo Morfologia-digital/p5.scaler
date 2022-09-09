fb80e39ed5ca1c2c4855c2b0d8a2b50ad70bfce1
Rodrigo Junqueira <oi.rodjun@gmail.com>
2022-09-09 17:53:46 -0300

    docs: :memo: Adds one sketch as an example

---
+++ examples/example01.js
@@ -0,0 +1,19 @@
+let frame = 0;
+
+function setup() {
+  createAdaptiveCanvas(2000, 2000);
+  strokeWeight(10);
+}
+
+function draw() {
+  background(200);
+  
+  rectMode(CENTER);
+  
+  translate(scaler.width() / 2, scaler.height() / 2);
+  rotate(radians(frame));
+  
+  rect(0, 0, scaler.width() / 2, scaler.height() / 2);
+  
+  frame++;
+}
