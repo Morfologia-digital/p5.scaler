2fd7a11c95c517d6e4772646e25e85c61e2fd1ff
Rodrigo Junqueira <oi.rodjun@gmail.com>
2022-11-09 19:49:46 -0300

    feat: :sparkles: Adds scaleCanvasTo() for printing

--- p5.scaler.js
+++ p5.scaler.js
@@ -1,11 +1,11 @@
 p5.prototype.scaler = undefined;
 p5.prototype.createAdaptiveCanvas = function (width, height, fitScreen) {
-  createCanvas(width, height);
+  let c = createCanvas(width, height);
   scaler = new Scaler(width, height, fitScreen === undefined ? true : false);
+  return c;
 };
 p5.prototype.updateAdaptiveCanvas = function () {
-  if (scaler !== undefined)
-    scale(scaler.scale());
+  if (scaler !== undefined) scale(scaler.scale());
 };
 window.addEventListener(
   'resize',
@@ -34,27 +34,35 @@ class Scaler {
 
   scaleCanvas() {
     if (this.#fitScreen) {
-      let scaledWidth, scaledHeight;
-      let diffWidth = Math.abs(this.#width - window.innerWidth);
-      let diffHeight = Math.abs(this.#height - window.innerHeight);
-      if (Math.min(diffWidth, diffHeight) == diffWidth) {
-        scaledWidth = window.innerWidth;
-        scaledHeight = window.innerWidth / this.#ratio;
-        if (scaledHeight > window.innerHeight) {
-          scaledHeight = window.innerHeight;
-          scaledWidth = window.innerHeight * this.#ratio;
-        }
-      } else {
-        scaledHeight = window.innerHeight;
-        scaledWidth = window.innerHeight * this.#ratio;
-        if (scaledWidth > window.innerWidth) {
-          scaledWidth = window.innerWidth;
-          scaledHeight = window.innerWidth / this.#ratio;
-        }
-      }
-      this.#scale = scaledWidth / this.#width;
-      resizeCanvas(scaledWidth, scaledHeight);
+      this._doScale(window.innerWidth, window.innerHeight);
     }
+  }
+
+  scaleCanvasTo(s) {
+    this._doScale(this.#width * s, this.#height * s);
+  }
+
+  _doScale(w, h) {
+    let scaledWidth, scaledHeight;
+    let diffWidth = Math.abs(this.#width - w);
+    let diffHeight = Math.abs(this.#height - h);
+    if (Math.min(diffWidth, diffHeight) == diffWidth) {
+      scaledWidth = w;
+      scaledHeight = w / this.#ratio;
+      if (scaledHeight > h) {
+        scaledHeight = h;
+        scaledWidth = h * this.#ratio;
+      }
+    } else {
+      scaledHeight = window.innerHeight;
+      scaledWidth = window.innerHeight * this.#ratio;
+      if (scaledWidth > w) {
+        scaledWidth = w;
+        scaledHeight = w / this.#ratio;
+      }
+    }
+    this.#scale = scaledWidth / this.#width;
+    resizeCanvas(scaledWidth, scaledHeight);
   }
 
   width() {
